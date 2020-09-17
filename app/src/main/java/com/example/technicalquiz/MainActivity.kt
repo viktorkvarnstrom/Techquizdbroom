f20013ae036e13c9d2a89793a2984a25589b8d8f
Viktor <vskvarnstrom@gmail.com>
2020-09-17 14:37:51 +0200

    First layout made and second activity started

--- app/src/main/java/com/example/technicalquiz/MainActivity.kt
+++ app/src/main/java/com/example/technicalquiz/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.example.technicalquiz
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
+
+        startQuiz_btn.setOnClickListener {
+            val intent = Intent(this, QuizActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
     }
 }
